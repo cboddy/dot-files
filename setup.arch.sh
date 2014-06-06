ea2f5626e13754d3752bc3272305b6c881a6c609
chrisb <chris.boddy@67bricks.com>
2014-06-06 14:35:23 +0100

    added arandr

--- setup.arch.sh
+++ setup.arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin)
+PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge)
 
 sudo pacman -Syy 
 for package in "${PACKAGES[@]}"
--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin)
+PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge)
 
 sudo apt-get update
 for package in "${PACKAGES[@]}"
