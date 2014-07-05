21f01a08d69815b103c3fa0d443a0cd1d603bd49
Chris Boddy <christoph.boddy@gmail.com>
2014-07-05 10:41:24 +0100

    aded git.

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge liferea)
+PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge liferea git)
 
 sudo apt-get update
 for package in "${PACKAGES[@]}"
