c900279bcd312c9bbe2add5723544255903702cf
Chris <chris@boddy.im>
2014-10-02 23:38:54 +0100

    Added curl, scala, sbt.

--- setup.arch.sh
+++ setup.arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge liferea git cifs-utils samba)
+PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge liferea git cifs-utils samba curl)
 
 sudo pacman -Syy 
 for package in "${PACKAGES[@]}"
--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge liferea git fpm2 cifs-utils samba vlc-nox default-jdk steam)
+PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge liferea git fpm2 cifs-utils samba vlc-nox default-jdk steam virtualbox-qt curl)
 
 sudo apt-get update
 for package in "${PACKAGES[@]}"
@@ -8,13 +8,35 @@ do
     sudo apt-get install $package
 done
 
+##
+##scala
+##
+
+wget www.scala-lang.org/files/archive/scala-2.11.1.deb
+sudo dpkg -i scala-2.11.1.deb
+ 
+# sbt installation
+wget wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.1/sbt.deb
+sudo dpkg -i sbt.deb
+## aliases
+
 cat bash/.bashrc >> ~/.bashrc
 
+##
+## vim
+##
 cp vim/.vimrc ~/.vimrc
 cp vim/.vim ~/
 
+#
+# vim file type plugins
+#
+mkdir -p ~/.vim/{ftdetect,indent,syntax} && for d in ftdetect indent syntax ; do wget --no-check-certificate -O ~/.vim/$d/scala.vim https://raw.githubusercontent.com/scala/scala-dist/master/tool-support/src/vim/$d/scala.vim; done
+
+##
+## fluxbox
+##
 cp fluxbox/menu ~/.fluxbox/
 
 cp fluxbox/init ~/.fluxbox/
 
-mkdir -p ~/.vim/{ftdetect,indent,syntax} && for d in ftdetect indent syntax ; do wget --no-check-certificate -O ~/.vim/$d/scala.vim https://raw.githubusercontent.com/scala/scala-dist/master/tool-support/src/vim/$d/scala.vim; done
