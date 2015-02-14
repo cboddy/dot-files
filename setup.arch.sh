42d0b495e9d61adade8c668c9956a754f89cd2b7
Chris Boddy <christoph.boddy@gmail.com>
2015-02-14 16:50:08 +0000

    Added zip to pacakges lists.

--- setup.arch.sh
+++ setup.arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge liferea git cifs-utils samba curl gitk openssh-sever visualvm)
+PACKAGES=(python openssh vim screen fluxbox clementime thunderbird firefox terminator pidgin tomboy xchat arandr deluge liferea git cifs-utils samba curl gitk openssh-sever visualvm zip)
 
 sudo pacman -Syy 
 for package in "${PACKAGES[@]}"
