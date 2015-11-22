a908fe597607c1143fb3b355fb98f860892df05a
Chris <chris@boddy.im>
2015-11-22 12:07:19 +0000

    Added zsh, i3 and updated sbt install.

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 ##
-##PPA for Oracle Java 8 installer
+## PPA for SBT
 ##
-
-
-##
-## List of packages to install
-##
-
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
 
 
 ##
 ## List of packages to install
 ##
-PACKAGES=(python openssh vim screen terminator git cifs-utils samba vlc-nox curl gitk openssh-server visualvm node npm zip oracle-java8-installer golang-go python-pip gnupg)
+PACKAGES=(python openssh vim screen terminator git cifs-utils samba vlc curl gitk openssh-server visualvm node npm zip golang-go python-pip gnupg i3 mdadm openjdk-8-jdk zsh)
 
-
-sudo apt-add-repository ppa:webupd8team/java
-sudo apt-get update
 for package in "${PACKAGES[@]}"
 do
     sudo apt-get -y install $package
@@ -38,31 +31,32 @@ wget www.scala-lang.org/files/archive/scala-2.11.1.deb
 sudo dpkg -i scala-2.11.1.deb
  
 # sbt installation
-wget wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.1/sbt.deb
-sudo dpkg -i sbt.deb
-## aliases
+sudo apt-get update
+sudo apt-get install sbt
 
+##
+## aliases for bash and zsh 
+##
 cat bash/.bashrc >> ~/.bashrc
+cat bash/.bashrc >> ~/.zshrc
 
 ##
 ## vim
 ##
 cp vim/.vimrc ~/.vimrc
 cp -r vim/.vim ~/
-
+#
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 
-vim +PluginInstall +qall
+#vim +PluginInstall +qall
 #ensure go plugin  stuff is installed
 vim -E -c GoInstallBinaries -c q
-##
-## fluxbox
-##
 
-mkdir ~/.fluxbox
 
-cp fluxbox/* ~/.fluxbox/
-
+#
+# Oh My ZSH
+#
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ##
 ## node stuff
