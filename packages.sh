94aded3aee2785143ce6090df5be71030d208398
Chris Boddy <chris@boddy.im>
2017-11-06 13:19:12 +0000

    added vim8

--- packages.sh
+++ packages.sh
@@ -6,6 +6,10 @@
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
 
+
+## vim8
+sudo add-apt-repository ppa:jonathonf/vim
+
 ##
 ## List of packages to install
 ##
