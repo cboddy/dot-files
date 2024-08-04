bd3686af5f52307cbe9e8d6b026c9a1c1b152e23
Chris Boddy <chris@boddy.im>
2024-08-04 22:11:05 +0100

    remove annoying git config message

--- shells.sh
+++ shells.sh
@@ -27,8 +27,12 @@ cp -r .git_templates ~/
 cp .gitignore ~/
 cp .git-prompt.sh ~/
 
+git config advice.addEmptyPathspec false
 
 ##
 ## i3
 ##
 cp -r i3 ~/.i3
+
+
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
